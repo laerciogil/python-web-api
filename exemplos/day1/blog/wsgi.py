b3199eae1af828a758389d3b66f644d75ac6acb8
Laercio Gil <laerciogilpb@gmail.com>
2026-05-10 19:01:36 -0300

    Add new files for wsgi form input lesson

--- exemplos/day1/blog/wsgi.py
+++ exemplos/day1/blog/wsgi.py
@@ -1,3 +1,4 @@
+import cgi
 from pathlib import Path
 from database import conn
 
@@ -24,6 +25,14 @@ def get_post_list(posts):
     ]
     return "\n".join(post_list)
 
+def add_new_post(post):
+    cursor = conn.cursor()
+    cursor.execute("""
+        INSERT INTO post (title, content, author) 
+        VALUES (:title, :content, :author);
+    """, post)
+    conn.commit()
+
 def application(environ, start_response):
     body = b"Content Not Found"
     status = "404 Not Found"
@@ -45,6 +54,19 @@ def application(environ, start_response):
             post=post[0]
         )
         status = "200 OK"
+    elif path == "/new" and method == "GET":
+        body = render_template("form.template.html")
+        status = "200 OK"
+    elif path == "/new" and method == "POST":
+        form = cgi.FieldStorage(
+            fp=environ["wsgi.input"],
+            environ=environ,
+            keep_blank_values=True
+        )
+        post = {item.name: item.value for item in form.list}
+        add_new_post(post)
+        body = b"New post created with success!"
+        status = "201 Created"
     
     # Montar a response
     start_response(status, headers)

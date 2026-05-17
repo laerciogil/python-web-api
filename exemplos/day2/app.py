5f39ad90ff4e7c6001af7553a56d6fdb0a78a797
Laercio Gil <laerciogilpb@gmail.com>
2026-05-17 03:17:18 -0300

    Add files for flask contexts lesson

--- exemplos/day2/app.py
+++ exemplos/day2/app.py
@@ -1,8 +1,24 @@
-from flask import Flask
+from flask import Flask, url_for, request
 
 app = Flask(__name__)
 
+app.config["APP_NAME"] = "Meu Blog"
+
+@app.errorhandler(404)
+def not_found_page(error):
+    return f"Not Found on {app.config["APP_NAME"]}"
+
 
 @app.route("/")
-def hello():
-    return "<strong>Hello, World!</strong>", 201
+def index():
+    content_url = url_for("read_content", slug="qualquer-coisa")
+    return (
+        f"<h1>Boas vindas a {app.config['APP_NAME']}</h1>"
+        f"<a href='{content_url}'>Leia um post</a>"
+        f"<hr/>"
+    )
+
+@app.route("/<slug>")
+def read_content(slug):
+    index_url = url_for("index")
+    return f"<h1>{slug}</h1><a href='{index_url}'>Voltar ao inicio</a>"

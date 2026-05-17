df941a3320b4bf0e19f0039ab953086eba77648b
Laercio Gil <laerciogilpb@gmail.com>
2026-05-17 18:49:35 -0300

    Create new files for flask cli lesson

---
+++ exemplos/day2/flask/blog/posts.py
@@ -0,0 +1,36 @@
+from blog.database import mongo
+from datetime import datetime
+
+
+def get_all_posts(published: bool = True) -> list[dict|None]:
+    posts = mongo.db.posts.find({"published": published})
+    return list(posts.sort("date"))
+
+
+def get_post_by_slug(slug: str) -> dict:
+    post = mongo.db.posts.find_one({"slug": slug})
+    return post
+
+
+def update_post_by_slug(slug: str, data: dict) -> dict:
+    # TODO: Se o título mudar, atualizar o slug (falhar se já existir)
+    return mongo.db.posts.find_one_and_update(
+        {"slug": slug}, 
+        {"$set": data}
+    )
+
+
+def new_post(title: str, content: str, published: bool = True) -> str:
+    # TODO: Refatorar a criação do slug removendo acentos
+    slug = title.replace(" ", "-").replace("_", "-").lower()
+    # TODO: Verificar se post com este slug já existe
+    new = mongo.db.posts.insert_one(
+        {
+            "title": title,
+            "content": content,
+            "published": published,
+            "slug": slug,
+            "date": datetime.now(),
+        }
+    )
+    return slug

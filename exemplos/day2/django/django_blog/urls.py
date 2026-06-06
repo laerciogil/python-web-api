0af9271154e72731a8402df290034a7c8dc3981d
Laercio Gil <laerciogilpb@gmail.com>
2026-06-06 00:43:02 -0300

    Create admin and auth logics for django blog

--- exemplos/day2/django/django_blog/urls.py
+++ exemplos/day2/django/django_blog/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from djblog.views import new_post, PostDetail, PostList
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls")),
     path("new/", new_post, name="new_post"),
     path("", PostList.as_view(), name="index"),
     path("<slug:slug>/", PostDetail.as_view(), name="detail")
--- exemplos/day2/django/djblog/admin.py
+++ exemplos/day2/django/djblog/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from djblog.models import Post
 
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'published', 'date')
+    list_filter = ('published', 'date')
+    search_fields = ('title', 'content')
+    prepopulated_fields = {'slug': ('title',)}
+    date_hierarchy = 'date'
+    ordering = ('-date',)
+    actions = ["publish"]
+
+    @admin.action(description="Publish/Unpublish posts")
+    def publish(self, request, queryset):
+        for post in queryset:
+            post.published = not post.published
+            post.save()
+
+admin.site.register(Post, PostAdmin)
 # Register your models here.
